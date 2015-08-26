03b977104d6fe01727e4f1bad5943498b44c6860
robertowtr <roberto.wtr@gmail.com>
2015-08-26 19:37:54 -0300

    Start

--- Cesar.py
+++ Cesar.py
@@ -3,34 +3,42 @@ import Util
 def start(fileName):
     option = Util.menuOperacao()
     if option == 1:
-        cript(fileName)
+        key = int(Util.scan("Key: "))
+        cript(fileName, key)
     elif option == 2:
         decript(fileName)
 
-def cript(fileName):
-    key = int(Util.scan("Key: "))
+def cript(fileName, key):
+
     inputFile = Util.scanFile(fileName)
     outputText = ""
 
-    for i in range(len(inputFile)):
-        asciiCode = ord(inputFile[i]) + key
-        if asciiCode > 255:
-            asciiCode = asciiCode - 255
+    for i in range(0, len(inputFile)):
+        asciiCode = (ord(inputFile[i]) + key) % 256
+        outputText = outputText + unichr(asciiCode).encode('Latin-1')#chr(asciiCode)
 
-        outputText = outputText + chr(asciiCode)
+    #print(outputText)
+    #Util.writeFile("./saidaCriptCesar.txt", outputText)
+    return outputText
 
-    Util.writeFile("./saida.txt", outputText)
+def criptCompare(inputFile, outputfile, key):
+    idx = 0
+    for i in range(0, len(inputFile)):
+        asciiCode = (ord(inputFile[i]) + key) % 256
+        if outputfile[idx] != chr(asciiCode):
+            return False
+        idx += 1
+    return True
 
 def decript(fileName):
     key = int(Util.scan("Key: "))
+    fileName = "saidaCriptCesar.txt"
     inputFile = Util.scanFile(fileName)
     outputText = ""
 
-    for i in range(len(inputFile)):
-        asciiCode = ord(inputFile[i]) - key
-        if asciiCode < 0:
-            asciiCode = asciiCode + 255
+    for i in range(0, len(inputFile)):
+        asciiCode = (ord(inputFile[i]) - key) % 256
+        outputText = outputText + unichr(asciiCode).encode('Latin-1')#chr(asciiCode)
 
-        outputText = outputText + chr(asciiCode)
-
-    Util.writeFile("./saida.txt", outputText)
+    print(outputText)
+    Util.writeFile("./saidaDecriptCesar.txt", outputText)
